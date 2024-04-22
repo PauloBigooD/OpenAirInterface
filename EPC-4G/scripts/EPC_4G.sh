0a3cb19e68e12fd73ae7fe8ea53a3414ae4c483e
PauloBigooD <53543357+PauloBigooD@users.noreply.github.com>
2024-04-22 14:04:54 -0300

    Update EPC_4G.sh

--- EPC-4G/scripts/EPC_4G.sh
+++ EPC-4G/scripts/EPC_4G.sh
@@ -25,104 +25,108 @@ case "${command}" in
  --logs    = EPC 4G logs"
 		;;
 	"--stop")
-                cd $WORK_DIR/openairinterface5g/ci-scripts/yaml_files/4g_rfsimulator_fdd_05MHz
-                sudo docker compose down
+            cd $WORK_DIR/openairinterface5g/ci-scripts/yaml_files/4g_rfsimulator_fdd_05MHz
+            sudo docker compose down
 		;;
 	"--install")
-                echo "OAI RAN/UE 4G/5G Installation"
-                sudo add-apt-repository ppa:ettusresearch/uhd
-                sudo apt-get install libuhd-dev uhd-host -y
-		sudo apt-get install libuhd4.2.0 -y && \
-		sudo dpkg -i --force-overwrite /var/cache/apt/archives/libuhd4.2.0_4.2.0.1-0ubuntu1~focal1_amd64.deb || \
-		sudo dpkg -i --force-overwrite /var/cache/apt/archives/libuhd4.2.0_4.2.0.1-0ubuntu1~focal1_amd64.deb
-                sudo apt-get install libuhd4.4.0 -y && \
-                sudo dpkg -i --force-overwrite /var/cache/apt/archives/libuhd4.4.0_4.4.0.0-0ubuntu1~focal1_amd64.deb || \
-		sudo dpkg -i --force-overwrite /var/cache/apt/archives/libuhd4.2.0_4.2.0.1-0ubuntu1~focal1_amd64.deb
+            echo "OAI RAN/UE 4G/5G Installation"
+            sudo add-apt-repository ppa:ettusresearch/uhd
+            sudo apt-get install libuhd-dev uhd-host -y
+            sudo apt-get install libuhd4.6.0 -y && \
+            sudo dpkg -i --force-overwrite /var/cache/apt/archives/libuhd4.6.0_4.6.0.0-0ubuntu1~focal1_amd64.deb || \
+            sudo dpkg -i --force-overwrite /var/cache/apt/archives/libuhd4.6.0_4.6.0.0-0ubuntu1~focal1_amd64.deb
+                
+            echo "Clone OpenAirInterface 5G RAN repository"
+            sudo apt-get install git
+            # Verificando se o repositório existe
+            if [ -d "openairinterface5g" ]; then
+                echo "Removendo o diretório existente 'openairinterface5g'..."
+                rm -rf openairinterface5g
+            fi
 
-                echo "Clone OpenAirInterface 5G RAN repository"
-		sudo apt-get install git
-		# Verificando se o repositório existe
-		if git ls-remote "$repo_url" > /dev/null 2>&1; then
-   		rm -rf  openairinterface5g
-		else
-   		git clone "$repo_url"
+            # Verifica novamente se o diretório foi removido com sucesso
+            if [ ! -d "openairinterface5g" ]; then
+                # Se não existir mais, clona o repositório
+                git clone "$repo_url"
                 cd $WORK_DIR/openairinterface5g
                 git checkout develop
-		fi
+            else
+                echo "Erro: Não foi possível remover o diretório 'openairinterface5g'."
+            fi
 		
-                echo "Install OAI dependencies and Build OAI UE and eNB"
-                source oaienv
-                cd cmake_targets
-                sudo ./build_oai -I
-                sudo ./build_oai -I --install-optional-packages
-                sudo ./build_oai -w USRP --ninja --eNB --UE -C
+            echo "Install OAI dependencies and Build OAI UE and eNB"
+            source oaienv
+            cd cmake_targets
+            sudo ./build_oai -I
+            sudo ./build_oai -I --install-optional-packages
+            sudo ./build_oai -w USRP --nrUE --eNB --UE -C --build-lib "telnetsrv enbscope uescope nrscope nrqtscope"
                 
-                echo "Uninstall Docker any such older versions before attempting to install a new version"
-                sudo apt-get remove docker docker-engine docker.io containerd runc 
-                ## Update the apt package index and install packages to allow apt to use a repository over HTTPS:
-                sudo apt-get update -y
-                sudo apt-get install ca-certificates curl gnupg -y
-                ## Add Docker’s official GPG key
-                sudo install -m 0755 -d /etc/apt/keyrings
-                curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-                sudo chmod a+r /etc/apt/keyrings/docker.gpg
-                ## set up the repository
-                echo \
-                "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-                "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-                sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+            echo "Uninstall Docker any such older versions before attempting to install a new version"
+            sudo apt-get remove docker docker-engine docker.io containerd runc 
+            ## Update the apt package index and install packages to allow apt to use a repository over HTTPS:
+            sudo apt-get update -y
+            sudo apt-get install ca-certificates curl gnupg -y
+            ## Add Docker’s official GPG key
+            sudo install -m 0755 -d /etc/apt/keyrings
+            curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+            sudo chmod a+r /etc/apt/keyrings/docker.gpg
+            ## set up the repository
+            echo \
+            "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+            "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+            sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
                 
-		echo "Install Docker Engine"
-                sudo apt-get update -y
-                sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+		    echo "Install Docker Engine"
+            sudo apt-get update -y
+            sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
                 
-		echo "Pulling the images from Docker Hub"
-                sudo docker login
-                sudo docker pull cassandra:2.1
-                sudo docker pull redis:6.0.5
-                sudo docker pull oaisoftwarealliance/oai-hss:latest
-                sudo docker pull oaisoftwarealliance/magma-mme:latest
-                sudo docker pull oaisoftwarealliance/oai-spgwc:latest
-                sudo docker pull oaisoftwarealliance/oai-spgwu-tiny:latest
-                sudo docker pull oaisoftwarealliance/oai-enb:develop
-                sudo docker pull oaisoftwarealliance/oai-lte-ue:develop
-                ## Re-tag
-                sudo docker image tag oaisoftwarealliance/oai-spgwc:latest oai-spgwc:latest
-                sudo docker image tag oaisoftwarealliance/oai-hss:latest oai-hss:latest
-                sudo docker image tag oaisoftwarealliance/oai-spgwu-tiny:latest oai-spgwu-tiny:latest
-                sudo docker image tag oaisoftwarealliance/magma-mme:latest magma-mme:latest
+		    echo "Pulling the images from Docker Hub"
+            sudo docker login
+            sudo docker pull cassandra:2.1
+            sudo docker pull redis:6.0.5
+            sudo docker pull oaisoftwarealliance/oai-hss:latest
+            sudo docker pull oaisoftwarealliance/magma-mme:latest
+            sudo docker pull oaisoftwarealliance/oai-spgwc:latest
+            sudo docker pull oaisoftwarealliance/oai-spgwu-tiny:latest
+            sudo docker pull oaisoftwarealliance/oai-enb:develop
+            sudo docker pull oaisoftwarealliance/oai-lte-ue:develop
+            ## Re-tag
+            sudo docker image tag oaisoftwarealliance/oai-spgwc:latest oai-spgwc:latest
+            sudo docker image tag oaisoftwarealliance/oai-hss:latest oai-hss:latest
+            sudo docker image tag oaisoftwarealliance/oai-spgwu-tiny:latest oai-spgwu-tiny:latest
+            sudo docker image tag oaisoftwarealliance/magma-mme:latest magma-mme:latest
 		;;
 	"--start")
-		## Performance mode
-		#sudo /etc/init.d/cpufrequtils restart
-                ## Configuration of the packer forwarding
-		sudo sysctl net.ipv4.conf.all.forwarding=1
-		sudo iptables -P FORWARD ACCEPT
-		echo "Deploy and Configure Cassandra Database"
-                cd $WORK_DIR/openairinterface5g/ci-scripts/yaml_files/4g_rfsimulator_fdd_05MHz
-                ## Un-deployment olds containers
-                sudo docker compose down
-                ## Deploy and Configure Cassandra Database
-                sudo docker compose up -d db_init
-                ## Run docker command in background
-		sudo docker logs rfsim4g-db-init --follow &
-		## Monitor docker command output
-		while :
-		do
-		    ## Checks if the command output contains "OK"
-		    if sudo docker logs rfsim4g-db-init | grep -q "OK"; then
-		        echo "Status OK!"
+		    ## Performance mode
+		    #sudo /etc/init.d/cpufrequtils restart
+            ## Configuration of the packer forwarding
+		    sudo sysctl net.ipv4.conf.all.forwarding=1
+		    sudo iptables -P FORWARD ACCEPT
+		    echo "Deploy and Configure Cassandra Database"
+            cd $WORK_DIR/openairinterface5g/ci-scripts/yaml_files/4g_rfsimulator_fdd_05MHz
+            ## Un-deployment olds containers
+            sudo docker compose down
+            ## Deploy and Configure Cassandra Database
+            sudo docker compose up -d db_init
+            ## Run docker command in background
+		    sudo docker logs rfsim4g-db-init --follow &
+		    ## Monitor docker command output
+		    while :
+		    do
+		        ## Checks if the command output contains "OK"
+		        if sudo docker logs rfsim4g-db-init | grep -q "OK"; then
+		            echo "Status OK!"
 	                sudo docker rm rfsim4g-db-init
-			## Deploy Magma-MME
+			        ## Deploy Magma-MME
         	        sleep 5
                		sudo docker compose up -d magma_mme oai_spgwu trf_gen
 	                ## Container list
-			sleep 5
+			        sleep 5
 	                sudo docker compose ps -a
-			break
-		    fi
-		    sleep 20
-		done
+			    break
+		        fi
+		        sleep 20
+		    done
 		;;
         "--eNB")
                 ## eNodeB Monolithic (USRP) deployment
