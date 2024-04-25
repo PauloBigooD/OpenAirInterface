f50490cea4a882c1a09be4079f78cc7991db2c77
PauloBigooD <53543357+PauloBigooD@users.noreply.github.com>
2024-04-25 12:07:29 -0300

    Update EPC_4G.sh

--- EPC-4G/scripts/EPC_4G.sh
+++ EPC-4G/scripts/EPC_4G.sh
@@ -26,19 +26,32 @@ case "${command}" in
 		;;
 	"--stop")
             cd $WORK_DIR/openairinterface5g/ci-scripts/yaml_files/4g_rfsimulator_fdd_05MHz
-            sudo docker compose down
+            sudo docker-compose down
 		;;
 	"--install")
             echo "OAI RAN/UE 4G/5G Installation"
             sudo add-apt-repository ppa:ettusresearch/uhd
-            sudo apt-get install libuhd-dev uhd-host -y
-            sudo apt-get install libuhd4.6.0 -y && \
-            sudo dpkg -i --force-overwrite /var/cache/apt/archives/libuhd4.6.0_4.6.0.0-0ubuntu1~focal1_amd64.deb || \
-            sudo dpkg -i --force-overwrite /var/cache/apt/archives/libuhd4.6.0_4.6.0.0-0ubuntu1~focal1_amd64.deb
-                
-            echo "Clone OpenAirInterface 5G RAN repository"
-            sudo apt-get install git
-            # Verificando se o repositório existe
+	    
+	    # Verifica se o pacote libuhd4.6.0 está instalado
+	    if dpkg -l | grep -q libuhd4.6.0; then
+    		echo "Pacote libuhd4.6.0 já instalado."
+	    else
+    		# Instala o pacote libuhd4.6.0
+    		sudo apt-get install libuhd-dev uhd-host -y
+		sudo apt-get install libuhd4.6.0 -y && \
+                sudo dpkg -i --force-overwrite /var/cache/apt/archives/libuhd4.6.0_4.6.0.0-0ubuntu1~focal1_amd64.deb || \
+                sudo dpkg -i --force-overwrite /var/cache/apt/archives/libuhd4.6.0_4.6.0.0-0ubuntu1~focal1_amd64.deb
+	    fi
+            
+	    if dpkg -l | grep -q git; then
+                echo "Pacote git já instalado."
+            else
+            	sudo apt-get install git -y
+	    fi
+
+	    echo "Clone OpenAirInterface 5G RAN repository"
+            
+	    # Verificando se o repositório existe
             if [ -d "openairinterface5g" ]; then
                 echo "Removendo o diretório existente 'openairinterface5g'..."
                 rm -rf openairinterface5g
@@ -76,11 +89,11 @@ case "${command}" in
             "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
             sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
                 
-		    echo "Install Docker Engine"
+	    echo "Install Docker Engine"
             sudo apt-get update -y
             sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
                 
-		    echo "Pulling the images from Docker Hub"
+	    echo "Pulling the images from Docker Hub"
             sudo docker login
             sudo docker pull cassandra:2.1
             sudo docker pull redis:6.0.5
@@ -97,21 +110,86 @@ case "${command}" in
             sudo docker image tag oaisoftwarealliance/magma-mme:latest magma-mme:latest
 		;;
 	"--start")
-		    ## Performance mode
-		    #sudo /etc/init.d/cpufrequtils restart
+	    ## Performance mode
+	    #sudo /etc/init.d/cpufrequtils restart
             ## Configuration of the packer forwarding
-		    sudo sysctl net.ipv4.conf.all.forwarding=1
-		    sudo iptables -P FORWARD ACCEPT
-		    echo "Deploy and Configure Cassandra Database"
+	    sudo sysctl net.ipv4.conf.all.forwarding=1
+	    sudo iptables -P FORWARD ACCEPT
+	    echo "Deploy and Configure Cassandra Database"
             cd $WORK_DIR/openairinterface5g/ci-scripts/yaml_files/4g_rfsimulator_fdd_05MHz
             ## Un-deployment olds containers
-            sudo docker compose down
+            sudo docker-compose down
             ## Deploy and Configure Cassandra Database
-            sudo docker compose up -d db_init
+            sudo docker-compose up -d db_init
             ## Run docker command in background
 		    sudo docker logs rfsim4g-db-init --follow &
 		    ## Monitor docker command output
-		    while :
+		    while :#!/bin/bash
+
+## Author:   Paulo Eduardo da Silva Junior - paulo.eduardo.093@ufrn.edu.br - Tel: +55 (84) 9 8808-0933
+## GitHub:   https://github.com/PauloBigooD
+## Linkedin: https://www.linkedin.com/in/paulo-eduardo-5a18b3174/
+
+set -e
+
+## Work directory path is the current directory
+WORK_DIR=$PWD
+
+## Definindo o URL do repositório
+repo_url="https://gitlab.eurecom.fr/oai/openairinterface5g.git"
+
+command="$1"
+case "${command}" in
+	"--help")
+		echo "Common Commands: 
+ --install = Install all dependences for EPC 4G
+ --start   = Start EPC 4G
+ --stop    = Stop EPC 4G  
+ --eNB     = Start eNB 4G
+ --eNB-sim = Start eNB 4G Simulada
+ --ue-sim  = Start UE 4G Simulado 
+ --logs    = EPC 4G logs"
+		;;
+	"--stop")
+            cd $WORK_DIR/openairinterface5g/ci-scripts/yaml_files/4g_rfsimulator_fdd_05MHz
+            sudo docker compose down
+		;;
+	"--install")
+            echo "OAI RAN/UE 4G/5G Installation"
+            sudo add-apt-repository ppa:ettusresearch/uhd
+            sudo apt-get install libuhd-dev uhd-host -y
+            sudo apt-get install libuhd4.6.0 -y && \
+            sudo dpkg -i --force-overwrite /var/cache/apt/archives/libuhd4.6.0_4.6.0.0-0ubuntu1~focal1_amd64.deb || \
+            sudo dpkg -i --force-overwrite /var/cache/apt/archives/libuhd4.6.0_4.6.0.0-0ubuntu1~focal1_amd64.deb
+                
+            echo "Clone OpenAirInterface 5G RAN repository"
+            sudo apt-get install git
+            # Verificando se o repositório existe
+            if [ -d "openairinterface5g" ]; then
+                echo "Removendo o diretório existente 'openairinterface5g'..."
+                rm -rf openairinterface5g
+            fi
+
+            # Verifica novamente se o diretório foi removido com sucesso
+            if [ ! -d "openairinterface5g" ]; then
+                # Se não existir mais, clona o repositório
+                git clone "$repo_url"
+                cd $WORK_DIR/openairinterface5g
+                git checkout develop
+            else
+                echo "Erro: Não foi possível remover o diretório 'openairinterface5g'."
+            fi
+		
+            echo "Install OAI dependencies and Build OAI UE and eNB"
+            source oaienv
+            cd cmake_targets
+            sudo ./build_oai -I
+            sudo ./build_oai -I --install-optional-packages
+            sudo ./build_oai -w USRP --nrUE --eNB --UE -C --build-lib "telnetsrv enbscope uescope nrscope nrqtscope"
+                
+            echo "Uninstall Docker any such older versions before attempting to install a new version"
+            sudo apt-get remove docker docker-engine docker.io containerd runc 
+
 		    do
 		        ## Checks if the command output contains "OK"
 		        if sudo docker logs rfsim4g-db-init | grep -q "OK"; then
@@ -119,10 +197,10 @@ case "${command}" in
 	                sudo docker rm rfsim4g-db-init
 			        ## Deploy Magma-MME
         	        sleep 5
-               		sudo docker compose up -d magma_mme oai_spgwu trf_gen
+               		sudo docker-compose up -d magma_mme oai_spgwu trf_gen
 	                ## Container list
 			        sleep 5
-	                sudo docker compose ps -a
+	                sudo docker-compose ps -a
 			    break
 		        fi
 		        sleep 20
@@ -133,16 +211,26 @@ case "${command}" in
                 cd $WORK_DIR/openairinterface5g/cmake_targets/ran_build/build/
                 sudo -E ./lte-softmodem -O ../../../ci-scripts/conf_files/enb.band7.100prb.usrpb200.tm1.conf
                 ;;
+        "--eNB-docker")
+                ## eNodeB Docker
+                cd $WORK_DIR/openairinterface5g/ci-scripts/yaml_files/4g_rfsimulator_fdd_05MHz
+                sudo docker-compose up -d oai_enb0
+		;;
+        "--ue-docker")
+                ## eNodeB Docker
+                cd $WORK_DIR/openairinterface5g/ci-scripts/yaml_files/4g_rfsimulator_fdd_05MHz
+                sudo docker-compose up -d oai_ue0
+                ;;
         "--eNB-sim")
                 ## eNodeB Simulator
-                cd $WORK_DIR/openairinterface5g/ci-scripts/yaml_files/4g_rfsimulator_fdd_05MHz
-                sudo docker compose up -d oai_enb0
-		;;
+                cd $WORK_DIR/openairinterface5g/cmake_targets/ran_build/build/
+                sudo -E ./lte-softmodem --rfsim --log_config.global_log_options level,nocolor,time -O ../../../ci-scripts/conf_files/enb.band7.25prb.rfsim.conf --eNBs.[0].rrc_inactivity_threshold 0 2>&1 -d
+                ;;
         "--ue-sim")
                 ## eNodeB Simulator
-                cd $WORK_DIR/openairinterface5g/ci-scripts/yaml_files/4g_rfsimulator_fdd_05MHz
-                sudo docker compose up -d oai_ue0
-                ;;
+                cd $WORK_DIR/openairinterface5g/cmake_targets/ran_build/build/
+                sudo -E ./lte-uesoftmodem --rfsim -C 2680000000 -r 25 --ue-rxgain 140 --ue-txgain 120 --rfsimulator.serveraddr 192.168.61.20 --log_config.global_log_options level,nocolor,time -O ../../../ci-scripts/conf_files/lteue.usim-ci.conf #-d
+		;;
         "--logs")
                 ## EPC logs
                 sudo docker exec -it rfsim4g-magma-mme /bin/bash -c "tail -f /var/log/mme.log"
